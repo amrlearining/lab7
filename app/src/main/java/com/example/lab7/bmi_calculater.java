447721d1c68c4e93f8b7cbb9e034400ee13759b1
amro <amryusef24@gmail.com>
2023-11-20 15:51:32 +0300

    complete program

--- app/src/main/java/com/example/lab7/bmi_calculater.java
+++ app/src/main/java/com/example/lab7/bmi_calculater.java
@@ -2,20 +2,41 @@ package com.example.lab7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Button;
+import android.view.View;
 public class bmi_calculater extends AppCompatActivity {
 TextView textView;
+EditText result, EditTextweight, EditTextheight;
+Button buttom;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi_calculater);
-        double result = 30;
-        bmiCat(result);
-    }
 
-    public void bmiCat(double num){
+        EditTextweight = findViewById(R.id.weight);
+        EditTextheight = findViewById(R.id.height);
+        buttom = findViewById(R.id.button2);
+        result = findViewById(R.id.result);
+        buttom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height=Float.parseFloat(String.valueOf(EditTextheight.getText()));
+                float weight=Float.parseFloat(String.valueOf(EditTextweight.getText()));
+                float bmi = weight / (height * height);
+                result.setText(String.valueOf(bmi), TextView.BufferType.EDITABLE);
+                bmiCat(bmi);
+            }
+        });
+
+
+    }
+    public void bmiCat(float num){
         textView = findViewById(R.id.cat);
 
         if (num < 18.5){
